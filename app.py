a4c4f3e11f9cbd55e594d5ddb9d60c74ecb4c10e
Aranhacorp <131683633+Aranhacorp@users.noreply.github.com>
2026-02-28 09:32:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ import random
 # ================= CONFIGURAÇÃO DA PÁGINA =================
 st.set_page_config(
     page_title="Userlog - Sistema de Transportes",
-    page_icon="📆",
+    page_icon="🔼",
     layout="wide",
     initial_sidebar_state="expanded"
 )
